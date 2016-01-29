0cfaf91c99991ef2819efd25a838a6eec6d47107
cristian <cristianv975@gmail.com>
2016-01-29 14:29:29 -0500

    menu

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    jade = require('gulp-jade'),
+    stylus = require('gulp-stylus'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    nib = require('nib');
+
+
+gulp.task('build:jade', function() {
+    gulp.src('./src/jade/*.jade')
+    .pipe(plumber())
+    .pipe(jade({
+        pretty: true
+        }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build:stylus', function() {
+    gulp.src('./src/stylus/*.styl')
+    .pipe(plumber())
+    .pipe(stylus({
+        use: nib(),
+        'include css': true
+        }))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('imgmin', function() {
+    return gulp.src('src/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('./src/jade/**/**/*.jade', ['build:jade']);
+    gulp.watch('./src/stylus/main.styl', ['build:stylus']);
+});
+
+
+gulp.task('default', ['build:jade', 'build:stylus', 'watch']);
